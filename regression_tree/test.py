0145f0f4f063a78ab581612816542e8605709b4f
zszew <zszwzszw@gmail.com>
2023-05-29 11:17:32 +0200

    added comparison with sklearn regressor

--- regression_tree/test.py
+++ regression_tree/test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+from sklearn.tree import DecisionTreeRegressor 
 
 import sys
 sys.path.append('../projektSI')  
@@ -12,14 +13,7 @@ from datasets.machine import prepareXY as machine
 from datasets.bone_marrow import prepareXY as bone_marrow
 from datasets.slump import prepareXY as slump
 
-from sklearn.tree import DecisionTreeRegressor 
-
-
-
- 
-
-
-for dataset in (automobile, wine, energy, machine, bone_marrow, slump):
+for dataset in (automobile, bone_marrow, energy, machine, slump, wine):
     (X, Y) = dataset.prepareXY()
     train_idx = random.sample(range(len(Y)), int(0.8 * len(Y)))
     X_train = [row for i, row in enumerate(X) if i in train_idx]
@@ -29,7 +23,6 @@ for dataset in (automobile, wine, energy, machine, bone_marrow, slump):
 
     rt = RegressionTree(X_train, Y_train)
 
-    SE = 0 #standard error of regression
     MSE = 0 #mean square error
     MAE = 0 #mean absolute error
    
@@ -43,54 +36,55 @@ for dataset in (automobile, wine, energy, machine, bone_marrow, slump):
     MAE /= len(Y_test)
     RMSE = np.sqrt(MSE)
 
-
-
-    print(f"{dataset.__name__}: MAE: {MAE}")
+    print(f"{dataset.__name__}: MAE: {MAE} RMSE: {RMSE}")
 
 
 
 #comparison with sklearn 
+print("COMPARISON WITH SCIKIT-LEARN")
+for dataset in (energy, slump, wine):
+    (X, Y) = dataset.prepareXY()
+    train_idx = random.sample(range(len(Y)), int(0.8 * len(Y)))
+    X_train = [row for i, row in enumerate(X) if i in train_idx]
+    Y_train = [row for i, row in enumerate(Y) if i in train_idx]
+    X_test =  [row for i, row in enumerate(X) if i not in train_idx]
+    Y_test =  [row for i, row in enumerate(Y) if i not in train_idx]
 
-(X, Y) = wine.prepareXY()
-train_idx = random.sample(range(len(Y)), int(0.8 * len(Y)))
-X_train = [row for i, row in enumerate(X) if i in train_idx]
-Y_train = [row for i, row in enumerate(Y) if i in train_idx]
-X_test =  [row for i, row in enumerate(X) if i not in train_idx]
-Y_test =  [row for i, row in enumerate(Y) if i not in train_idx]
+    rt = RegressionTree(X_train, Y_train)
+    ct = DecisionTreeRegressor(random_state=0) #compared tree
+    ct.fit(X_train, Y_train)
+    Cpredictions = ct.predict(X_test)
 
-rt = RegressionTree(X_train, Y_train)
+    rMSE = 0
+    rMAE = 0
+    cMSE = 0
+    cMAE = 0 
 
-ct = DecisionTreeRegressor(random_state=0)
-ct.fit(X_train, Y_train)
-
-
-
-rMSE = 0 #mean square error
-rMAE = 0 #mean absolute error
-
-cMSE = 0
-cMAE = 0 
-
-Cpredictions = ct.predict(X_test)
-   
-
-for x, y, c in zip(X_test, Y_test, Cpredictions):
-    Rprediction = rt.predict(x)
-            
-    rMAE += abs(y - prediction)
-    rMSE += (y - prediction)**2      
+    for x, y, c in zip(X_test, Y_test, Cpredictions):
+        Rprediction = rt.predict(x)
+              
+        rMAE += abs(y - Rprediction)
+        rMSE += (y - Rprediction)**2         
+        cMAE += abs(y - c)
+        cMSE += (y - c)**2 
+        
+    cMSE /= len(Y_test)
+    cMAE /= len(Y_test)
     rMSE /= len(Y_test)
     rMAE /= len(Y_test)
 
 
-    cMAE += abs(y - c)
-    cMSE += (y - c)**2 
-    cMSE /= len(Y_test)
-    cMAE /= len(Y_test)
-    
-rRMSE = np.sqrt(rMSE)
+    print(f"{dataset.__name__}:\nErrors:\nMAE: {rMAE}\nRMSE: {np.sqrt(rMSE)}\nSKlearn: \nMAE: {cMAE}\nRMSE: {np.sqrt(cMSE)}")
 
 
-print(f"{wine.__name__}:\n Error:\nMAE: {rMAE}\n MSE: {rMSE}\nFor comparison: \nMAE: {cMAE}\n MSE: {cMSE}")
+
+
+
+
+
+
+   
+
+
 
 
